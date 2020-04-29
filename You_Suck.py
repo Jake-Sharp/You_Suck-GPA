6d9ca27b59683f3294d80ea3cb4d2d7e9c54e3da
Jacob Sneed <jake sharp>
2020-04-28 21:39:55 -0500

    1.2

--- You_Suck.py
+++ You_Suck.py
@@ -1,6 +1,34 @@
-def YouSuck(a, b, list):
-    print(a + b)
+#Choice Betweem 4.0 and % systems
+def choice():
+    print("CHOOSE ALGORITHM")
+    print("1 - 4.0 System")
+    print("2 - Percent Standard")
+    firstChoice = ' '
+    while firstChoice != '1' or firstChoice !='2':
+        firstChoice = input()
+        if firstChoice == '1':
+            print("You Chose the 4.0 System")
+            fourPoint()
+        if firstChoice == '2':
+            print("You chose Percent Standard")
+            percent()
 
-YouSuck(12, 84, 'abc')
+#4.0 System Input
+def fourPoint():
+    #ask for numer of classes
+    #ask for grade input
 
-#(Just creating a starting platform)
+    algOne() #4.0 Equation
+
+#Percent Standard Input
+def percent():
+    #ask for numer of classes
+    #ask for grade input
+    
+    algTwo() #Percent Equation
+
+def algOne():
+    #I'll research this tomorrow
+    
+def algTwo():
+    #I'll research this tomorrow
